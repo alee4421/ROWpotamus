304fe97ab346c02df2858b70df9e95aeb654d2ec
alee4421 <alexandra.lee@greenwichschools.org>
2016-12-16 10:15:33 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -12,7 +12,7 @@ var today;
 var date = new Date();
 	var year = date.getFullYear();
 	var month = date.getMonth();
-	var day = (String)date.getDate();
+	var day = date.getDate();
 
 	var today = month + day + year;
 
