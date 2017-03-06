9af98348f3547fde95fe4d65ee4298f5815c2f17
alee4421 <alexandra.lee@greenwichschools.org>
2017-03-06 11:14:22 -0500

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -11,7 +11,7 @@ var today
 var rowNum = 1;
 var date = new Date();
 	var year = date.getFullYear();
-	var month = date.getMonth();
+	var month = date.getMonth() + 1;
 	var day = date.getDate();
 
 	var today = month +  "/" + day + "/" + year;
